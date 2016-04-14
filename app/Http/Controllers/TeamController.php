110df5644a29020e3a6316071184df979cce2be2
Cat Stefanovici <cat@sportsrecruits.com>
2016-04-14 18:02:41 -0400

    reverted a rule

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -104,19 +104,20 @@ class TeamController extends Controller
 
 	        break;
 	        case 'join':
-
+			default:
 				$user = Auth::user();
 				// check that they do not have a game in progress
-// 				$user = \App\User::matchInProgress->find(Auth::user()->id); // NOTE: left here
+/*
 				$user_in_progress = \App\MatchTeamsUser::whereHas('user', function ($query) use ($user) {
 					$query->where('user_id', $user->id);
 				})->whereHas('team.match', function ($query) {
-					$query->where('locked_winner_match_team_id', 0);
+					$query->where('locked_winner_match_team_id', '!=', 0);
 				})->with('team.match')->first();
-
+dd($user_in_progress);
 				if ($user_in_progress) {
 					return redirect('match/'.$user_in_progress->team->match->id)->with('message', 'Please select a winner for this match');
 				}
+*/
 
 				// redirect to that match if they have in progress with flash message saying 'please choose a winner first' 
 				
@@ -126,6 +127,7 @@ class TeamController extends Controller
 				$save_user->user_id = $user->id;
 				$save_user->save();
 	        	$team->users()->save($save_user);
+	        	
 	        break;
         }
         
