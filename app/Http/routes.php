b562d8c36e9e167f3c9619ebcc178f6114b96302
Anthony Rodriguez <Anthony@sportsrecruits.com>
2016-04-14 14:48:18 -0400

    integrate user view with blade

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,40 +9,40 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
 Route::get('/test', function () {
 
-	echo Carbon\Carbon::now('America/New_York');
-	
+    echo Carbon\Carbon::now('America/New_York');
+
 });
 
-
+Route::get('/userX', function () {
+    return view('user');
+});
 
 Route::get('/', 'LeaderboardController@index');
 
+Route::group(['middleware' => 'auth'], function () {
+    // creates a competition if necesarry
+    // displays the leaderboard
+    // loads up all of the players that have any points
 
-Route::group(['middleware' => 'auth'], function()
-{
-	// creates a competition if necesarry
-	// displays the leaderboard
-	// loads up all of the players that have any points
-	
-	Route::resource('match', 'MatchController');
-	Route::post('match/win/{id}', 'MatchController@win');
-	// REST matches
-		// save match_winners
-	
-	Route::resource('team', 'TeamController');
-	// REST teams
-		// add players to teams
-	
-	Route::get('user/', 'UserController@showProfile');
+    Route::resource('match', 'MatchController');
+    Route::post('match/win/{id}', 'MatchController@win');
+    // REST matches
+    // save match_winners
+
+    Route::resource('team', 'TeamController');
+    // REST teams
+    // add players to teams
+
+    Route::get('user/', 'UserController@showProfile');
 });
 
 Route::get('login', 'UserController@login');
 Route::get('logout', function () {
-	Auth::logout();
+    Auth::logout();
 });
 
 Route::get('oauth_callback/', 'UserController@handleProviderCallback');
